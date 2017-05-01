a87a5689ff1db4442414e0ff38acab4c4f4359e4
Noah Wisch <noah.wisch@gmail.com>
2017-04-30 20:20:17 -0400

    setting up react example

--- react-demo/app.js
+++ react-demo/app.js
@@ -1,1 +1,11 @@
 console.log('Hello World!');
+
+const jsContainer = document.getElementById('container');
+
+jsContainer.innerHTML = `
+  <div class="demo">
+    Hello World: The Sequel
+  </div>
+`;
+
+const reactContainer = document.getElementById("reactContainer");
